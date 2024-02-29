874185eacfa04b4d96c1c3460b291ddb1416a0f2
consoleforce <146631783+consoleforce@users.noreply.github.com>
2024-02-29 23:17:11 +0600

    Add files via upload

---
+++ services/contact/internal/domain/contact/contact.go
@@ -0,0 +1,13 @@
+package domain
+
+type Contact struct {
+	ID          int
+	FirstName   string
+	LastName    string
+	MiddleName  string
+	PhoneNumber string
+}
+
+func (c *Contact) FullName() string {
+	return c.LastName + " " + c.FirstName + " " + c.MiddleName
+}
